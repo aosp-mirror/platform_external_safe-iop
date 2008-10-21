a09f917d93140479fd7964892dd38d3a86c42b7a
The Android Open Source Project <initial-contribution@android.com>
2008-10-21 07:00:00 -0700

    Initial Contribution

--- include/safe_iop.h
+++ include/safe_iop.h
@@ -2,7 +2,7 @@
  * License:: released in to the public domain
  * Author:: Will Drewry <redpig@dataspill.org>
  * Copyright 2007,2008 redpig@dataspill.org
- * Some portions copyright Google Inc, 2008.
+ * Some portions copyright The Android Open Source Project
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
